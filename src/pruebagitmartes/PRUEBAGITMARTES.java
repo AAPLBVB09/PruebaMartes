067ebdf1aff49bc0249f1eff0d43db648d03dff8
silviachaparro <silvia_11715@hotmail.com>
2016-01-19 12:26:43 -0700

    Update PRUEBAGITMARTES.java
    cambyo

--- src/pruebagitmartes/PRUEBAGITMARTES.java
+++ src/pruebagitmartes/PRUEBAGITMARTES.java
@@ -15,6 +15,8 @@ public class PRUEBAGITMARTES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.PrintIn("debemos poder")
         // TODO code application logic here
     }
     
